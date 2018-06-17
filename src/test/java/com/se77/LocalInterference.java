d4d3128ea538bfc06a1145aedc6aa73eee55b4b8
superernie77 <superernie77@gmail.com>
2018-06-17 12:06:23 +0200

    Interference test + Maven project setup

---
+++ src/test/java/com/se77/LocalInterference.java
@@ -0,0 +1,31 @@
+package com.se77;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class LocalInterference {
+	
+	@Test
+	public void interference() {
+	
+		// Creates a List<String>
+		var myList = new ArrayList<String>();
+		
+		assertNotNull(myList);
+		assertTrue( myList instanceof List);
+		
+		// Creates a List<Object>
+		var myObjectList = new ArrayList<>();
+		
+		assertNotNull(myObjectList);
+	
+		
+		
+		
+	}
+
+}
